bc2eac6bd7e07908a69bafe89722fda947f594cf
Nevens-fr <aurelientudo@hotmail.fr>
2021-02-17 11:53:14 +0100

    Ajout d'un fond sur le menu, boutons non intégrés

--- Tests/Ecran_menu.rb
+++ Tests/Ecran_menu.rb
@@ -6,15 +6,20 @@ class Ecran_menu
 
     private_class_method :new
 
+    ##
+    #
+    ##
+    # * +win+   La fenetre de l'application
     def initialize(win)
         @win = win
-        @boite = Gtk::Box.new(:vertical, 5)
+        @boite = Gtk::Fixed.new()
+        @boite1 = Gtk::Box.new(:vertical)
 
         @jouer = Gtk::Button.new(:label => "Jouer")
         @quitter = Gtk::Button.new(:label => "Quitter")
 
-        @boite.add(@jouer)
-        @boite.add(@quitter)
+        @boite.add(Gtk::Image.new("../maquettes/menu.png"))
+        @boite1.add(@boite)
 
         @quitter.signal_connect("clicked"){
             Gtk.main_quit
@@ -23,12 +28,28 @@ class Ecran_menu
             vers_jeu()
         }
 
-        @win.add(@boite)
+        truc = Gtk::CssProvider.new
+
+        truc.load(data: <<-CSS)
+        button {
+            background-image: image(white);
+            color: black;
+        }
+        CSS
+
+        @jouer.style_context.add_provider(truc, Gtk::StyleProvider::PRIORITY_USER)
+        @quitter.style_context.add_provider(truc, Gtk::StyleProvider::PRIORITY_USER)   
+
+        #@boite.put(@jouer, 500, 500)
+
+        @win.add(@boite1)
 
         @win.show_all
         Gtk.main
     end
 
+    ##
+    # Permet de changer la fenetre pour aller afficher l'écran de jeu
     def vers_jeu()
         @win.remove(@boite)
         Ecran_jeu.creer(@win)
