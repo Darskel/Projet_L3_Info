dea42f921e184482bde9b47f16967e3b4f89d507
Nevens-fr <aurelientudo@hotmail.fr>
2021-02-17 14:51:43 +0100

    Intégration des boutons du menu, boutons options et tuto sans effet pour le moment, pb avec le bouton quitter

--- Tests/Ecran_menu.rb
+++ Tests/Ecran_menu.rb
@@ -1,4 +1,5 @@
 class Ecran_menu
+    
 
     def Ecran_menu.creer(win)
         new(win)
@@ -12,35 +13,91 @@ class Ecran_menu
     # * +win+   La fenetre de l'application
     def initialize(win)
         @win = win
+
         @boite = Gtk::Fixed.new()
         @boite1 = Gtk::Box.new(:vertical)
 
-        @jouer = Gtk::Button.new(:label => "Jouer")
-        @quitter = Gtk::Button.new(:label => "Quitter")
+        @jouer = Gtk::Button.new(:label => "")
+        @tuto = Gtk::Button.new(:label => "")
+        @option = Gtk::Button.new(:label => "")
+        @quitter = Gtk::Button.new(:label => "")
 
-        @boite.add(Gtk::Image.new("../maquettes/menu.png"))
+        @boite.add(Gtk::Image.new(:file => "../maquettes/menu.png"))
         @boite1.add(@boite)
 
+        ##
+        # Ajout des signaux des boutons
         @quitter.signal_connect("clicked"){
+            exit(true)
             Gtk.main_quit
         }
         @jouer.signal_connect("clicked"){
             vers_jeu()
         }
 
-        truc = Gtk::CssProvider.new
+        ##
+        # Création des CSS pour les boutons 
+        jouerImage = Gtk::CssProvider.new
 
-        truc.load(data: <<-CSS)
+        jouerImage.load(data: <<-CSS)
         button {
-            background-image: image(white);
-            color: black;
+            background-image: url("../maquettes/image18-3.png");
+            border: unset;
         }
         CSS
 
-        @jouer.style_context.add_provider(truc, Gtk::StyleProvider::PRIORITY_USER)
-        @quitter.style_context.add_provider(truc, Gtk::StyleProvider::PRIORITY_USER)   
+        tutoImage = Gtk::CssProvider.new
 
-        #@boite.put(@jouer, 500, 500)
+        tutoImage.load(data: <<-CSS)
+        button {
+            background-image: url("../maquettes/image18-5.png");
+            border: unset;
+        }
+        CSS
+
+        optionImage = Gtk::CssProvider.new
+
+        optionImage.load(data: <<-CSS)
+        button {
+            background-image: url("../maquettes/image18.png");
+            border: unset;
+        }
+        CSS
+
+        quitterImage = Gtk::CssProvider.new
+
+        quitterImage.load(data: <<-CSS)
+        button {
+            background-image: url("../maquettes/quitter.png");
+            border: unset;
+        }
+        CSS
+
+        widthOptionsPrincipales = 1134
+        heightOptionsPrincipales = 215
+        width = 438
+        height = 101
+
+        ##
+        # Ajout du CSS aux bouton et on leur donne leur taille
+        @jouer.style_context.add_provider(jouerImage, Gtk::StyleProvider::PRIORITY_USER)
+        @jouer.set_size_request(widthOptionsPrincipales, heightOptionsPrincipales)
+
+        @option.style_context.add_provider(optionImage, Gtk::StyleProvider::PRIORITY_USER)  
+        @option.set_size_request(widthOptionsPrincipales, heightOptionsPrincipales) 
+
+        @tuto.style_context.add_provider(tutoImage, Gtk::StyleProvider::PRIORITY_USER)  
+        @tuto.set_size_request(widthOptionsPrincipales, heightOptionsPrincipales) 
+
+        @quitter.style_context.add_provider(quitterImage, Gtk::StyleProvider::PRIORITY_USER)  
+        @quitter.set_size_request(width, height) 
+
+        ##
+        #Ajout des boutons et box dans les containers
+        @boite.put(@jouer, (Gdk::Screen.default.width / 2) - widthOptionsPrincipales*1.40, 300)
+        @boite.put(@tuto, (Gdk::Screen.default.width / 2) - widthOptionsPrincipales*1.40, 500)
+        @boite.put(@option, (Gdk::Screen.default.width / 2) - widthOptionsPrincipales*1.40, 700)
+        @boite.put(@quitter, (Gdk::Screen.default.width * 0.67- widthOptionsPrincipales) - 10, Gdk::Screen.default.height * 0.88)
 
         @win.add(@boite1)
 
@@ -51,7 +108,7 @@ class Ecran_menu
     ##
     # Permet de changer la fenetre pour aller afficher l'écran de jeu
     def vers_jeu()
-        @win.remove(@boite)
+        @win.remove(@boite1)
         Ecran_jeu.creer(@win)
     end
 end
