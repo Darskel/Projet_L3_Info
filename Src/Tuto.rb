1d279e65f7dcadd60bfd200dfa7fc64dd4cd888d
Guillaume <laukagui@gmail.com>
2021-03-10 12:09:13 +0100

    Mise en place boutons
    Mise en place boutons retourMenu ( flèche ), boutonBack ( coupPrécédent ) et boutonOptions ( options )

--- Src/Tuto.rb
+++ Src/Tuto.rb
@@ -9,19 +9,32 @@ class Tuto
     end
 
     def initialize(win)
+        #Création de l'interface 
         @window = win
-
         @box = Gtk::Fixed.new()
         @box2 = Gtk::Box.new(:horizontal)
-
         @suivant = Gtk::Button.new(:label => "Règle suivante")
         @retourMenu = Gtk::Button.new()
         @boutonBack = Gtk::Button.new()
         @boutonOptions = Gtk::Button.new()
 
+        #Css des boutons pour les rendres invisibles
         retourMenucss = Gtk::CssProvider.new()
-
+        backCss = Gtk::CssProvider.new()
+        optionsCss = Gtk::CssProvider.new()
         retourMenucss.load(data: <<-CSS)
+        button {
+            opacity: 0;
+            border: unset;
+        }
+        CSS
+        optionsCss.load(data: <<-CSS)
+        button {
+            opacity: 0;
+            border: unset;
+        }
+        CSS
+        backCss.load(data: <<-CSS)
         button {
             opacity: 0;
             border: unset;
@@ -31,18 +44,28 @@ class Tuto
         @retourMenu.style_context.add_provider(retourMenucss, Gtk::StyleProvider::PRIORITY_USER)
         @retourMenu.set_size_request(55, 45)
         @retourMenu.signal_connect("clicked"){
-            Utils.vers_menu(@window)
+            vers_menu(@window,@box2)
         }
-        #@boutonBack.set_size_request(widthOptionsPrincipales, heightOptionsPrincipales) 
-        #@boutonOptions.set_size_request(widthOptionsPrincipales, heightOptionsPrincipales) 
+        
+        @boutonOptions.style_context.add_provider(optionsCss, Gtk::StyleProvider::PRIORITY_USER)
+        @boutonOptions.set_size_request(60, 60)
+        @boutonOptions.signal_connect("clicked"){
+            #FonctionQuiOuvreLesOptions
+        }
+
+        @boutonBack.style_context.add_provider(backCss, Gtk::StyleProvider::PRIORITY_USER)
+        @boutonBack.set_size_request(60, 60)
+        @boutonBack.signal_connect("clicked"){
+            #FonctionQuifaitLeRetourArriere
+        }
 
         @box.add(Gtk::Image.new(:file => "../maquettes/TutorielV2.png"))
         @box2.add(@box)
 
         @box.put(@retourMenu,(1200 *0.015), 675 * 0.025)
         @box.put(@suivant,(1200 *0.85), 675 * 0.5)
-        @box.put(@boutonBack, 1200*0.8, 675*0)
-        @box.put(@boutonOptions,1200*0.95, 675*0.03)
+        @box.put(@boutonOptions, 1200*0.94, 675*0.02)
+        @box.put(@boutonBack,1200*0.89, 675*0.02)
         
         @window.add(@box2)
         @window.show_all
--- Src/Utils.rb
+++ Src/Utils.rb
@@ -1,11 +1,7 @@
-class Utils
-        
     ##
     # Permet de passer à l'écran du menu
-    def vers_menu(window)
+    def vers_menu(window,boite)
         @win = window
-        @win.remove(@boite)
+        @win.remove(boite)
         @ecr  = Ecran_menu.creer(@win)
     end
-
-end
