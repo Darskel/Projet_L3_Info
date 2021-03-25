d6e17c2e3248d22f7a72ebf36b717b85b10b49d0
taous06 <marouf-taous@hotmail.com>
2021-03-26 00:01:48 +0100

    Update Quicksave.rb

--- Src/Quicksave.rb
+++ Src/Quicksave.rb
@@ -11,21 +11,35 @@ private_class_method:new
     new(plateau, ha)
   end
 
-  def initialize (plateau, ha)
-    @plateau = plateau
+  def initialize ( plateau, ha )
 
-  
+        @plateau = plateau
+
+        tailleX = plateau.size()
+        tailleY = plateau[0].size()
+
+        @savePlateau = Array.new( ( 4 + 3 * (tailleX - 1 )) + ( (3 + 2 * ( tailleX - 1 )) * ( tailleY - 1 )), nil)
+
+        0.upto( tailleX - 1 ) { |i|
+            @savePlateau[i] = Array.new( tailleY, nil)
+        }
 
 
 
+        @saveHistorique = Historique.new()
+        savePos = ha.getPos()
 
+        begin
+            while ( true ) do
+                ha.undo()
+            end
+        rescue EmptyHistoriqueError => e
+            # on boucle jusqu'a ce que l'historique soit vide
+        end
 
-
-
-
-
-
-
+        @saveHistorique.setPos( savePos)
+        ha.setPos( savePos)
+    end
 
 
 end
